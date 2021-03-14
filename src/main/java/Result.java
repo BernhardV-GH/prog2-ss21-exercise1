287b7bb01575e6268a1f41215b65c2dca617dcaf
Bernhard Vogler <bernhard.vogler@stud.fh-campuswien.ac.at>
2021-03-14 15:01:29 +0100

    initial commit

---
+++ src/main/java/Result.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Result {
+    public static List<Integer> gradingStudents(List<Integer> grades){
+
+        return new ArrayList<>();
+    }
+}
