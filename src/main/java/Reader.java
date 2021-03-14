820f61ea49e81bb6d76ca9f86124f05081e03df6
Bernhard Vogler <bernhard.vogler@stud.fh-campuswien.ac.at>
2021-03-14 16:44:35 +0100

    Reader implementation done

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,8 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Reader {
-    public static List<Integer> readFromInput(String filepath) {
-        return new ArrayList<>();
+    public static List<Integer> readFromInput(String filepath){
+        String line = null;
+        List<Integer> input = new ArrayList<>();
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while(true){
+            try {
+                if ((line = br.readLine()) == null)
+                    break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            input.add(Integer.parseInt(line));
+        }
+
+        return input;
     }
 }
--- src/test/java/ReaderTest.java
+++ src/test/java/ReaderTest.java
@@ -42,7 +42,7 @@ class ReaderTest {
     @Test
     @DisplayName("ReaderTest5: inputTest3: n")
     void readerTest5(){
-        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest1.txt");
+        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest3.txt");
         int actual = (int)inputTest1.get(0);
         int expected = 61;
         assertEquals(actual,expected);
@@ -50,7 +50,7 @@ class ReaderTest {
     @Test
     @DisplayName("ReaderTest6: inputTest3: length")
     void readerTest6(){
-        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest1.txt");
+        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest3.txt");
         int actual = inputTest1.size();
         int expected = 62;
         assertEquals(actual,expected);
