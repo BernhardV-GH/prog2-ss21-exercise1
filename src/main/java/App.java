2be41a11b2de95a6ed511073492cbe5093befa9e
Bernhard Vogler <bernhard.vogler@stud.fh-campuswien.ac.at>
2021-03-15 17:24:10 +0100

    documentation and app class finished

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,19 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args){
 
+        System.out.println("Input File 1");
+        List<Integer> grades = Reader.readFromInput("src/main/resources/input1.txt");
+        System.out.println("source = " + grades);
+        List<Integer> results = Result.gradingStudents(grades);
+        System.out.println("result = " + results);
+        System.out.println();
+
+        System.out.println("Input File 2");
+        grades = Reader.readFromInput("src/main/resources/input2.txt");
+        System.out.println("source = " + grades);
+        results = Result.gradingStudents(grades);
+        System.out.println("result = " + results);
     }
 }
--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -3,6 +3,10 @@ import java.util.List;
 
 public class Result {
     public static List<Integer> gradingStudents(List<Integer> grades){
+        // This method takes a list of grades (first entry has to be the arraylength of the following entries)
+        // If the list is either too short or too long null is returned otherwise the array is modified using modulo 5
+        // as was specified. The first entry (n) is removed during this process. It isn't needed -> use .size() instead
+
         List<Integer> results = new ArrayList<>();
 
         if (grades.get(0) < 1 || grades.get(0) > 60){
