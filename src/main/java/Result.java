6008523880aa705aa0242e5b3973ecd31d263a1e
Bernhard Vogler <bernhard.vogler@stud.fh-campuswien.ac.at>
2021-03-15 16:25:51 +0100

    refactoring/renaming grading class done

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -3,7 +3,34 @@ import java.util.List;
 
 public class Result {
     public static List<Integer> gradingStudents(List<Integer> grades){
+        List<Integer> results = new ArrayList<>();
 
-        return new ArrayList<>();
+        if (grades.get(0) < 1 || grades.get(0) > 60){
+            System.out.println("file is either empty (n < 1) or too long (n > 60)");
+            return null;
+        }
+        else{
+            grades.remove(0);
+            for(int i = 0; i < grades.size(); i++){
+                if(grades.get(i) < 0 || grades.get(i) > 100){
+                    System.out.println("entry " + (i+1) + " is either '< 0' or '> 100'. Invalid entry set to '-1'");
+                    results.add(-1);
+                }
+                else if(grades.get(i) > 0 && grades.get(i) < 40){
+                    results.add(grades.get(i));
+                }
+                else if((5 - grades.get(i) % 5) == 2){
+                    results.add(grades.get(i)+2);
+                }
+                else if((5 - grades.get(i) % 5) == 1){
+                    results.add(grades.get(i)+1);
+                }
+                else{
+                    results.add(grades.get(i));
+                }
+            }
+        }
+
+        return results;
     }
 }
--- src/test/java/ReaderTest.java
+++ src/test/java/ReaderTest.java
@@ -8,7 +8,7 @@ class ReaderTest {
     @Test
     @DisplayName("ReaderTest1: inputTest1: n")
     void readerTest1(){
-        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest1.txt");
+        List inputTest1 = Reader.readFromInput("src/test/resources/readerTest1.txt");
         int actual = (int)inputTest1.get(0);
         int expected = 7;
         assertEquals(actual,expected);
@@ -16,7 +16,7 @@ class ReaderTest {
     @Test
     @DisplayName("ReaderTest2: inputTest1: length")
     void readerTest2(){
-        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest1.txt");
+        List inputTest1 = Reader.readFromInput("src/test/resources/readerTest1.txt");
         int actual = inputTest1.size();
         int expected = 8;
         assertEquals(actual,expected);
@@ -25,7 +25,7 @@ class ReaderTest {
     @Test
     @DisplayName("ReaderTest3: inputTest2: n")
     void readerTest3(){
-        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest2.txt");
+        List inputTest1 = Reader.readFromInput("src/test/resources/readerTest2.txt");
         int actual = (int)inputTest1.get(0);
         int expected = 10;
         assertEquals(actual,expected);
@@ -33,7 +33,7 @@ class ReaderTest {
     @Test
     @DisplayName("ReaderTest4: inputTest2: length")
     void readerTest4(){
-        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest2.txt");
+        List inputTest1 = Reader.readFromInput("src/test/resources/readerTest2.txt");
         int actual = inputTest1.size();
         int expected = 11;
         assertEquals(actual,expected);
@@ -42,7 +42,7 @@ class ReaderTest {
     @Test
     @DisplayName("ReaderTest5: inputTest3: n")
     void readerTest5(){
-        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest3.txt");
+        List inputTest1 = Reader.readFromInput("src/test/resources/readerTest3.txt");
         int actual = (int)inputTest1.get(0);
         int expected = 61;
         assertEquals(actual,expected);
@@ -50,7 +50,7 @@ class ReaderTest {
     @Test
     @DisplayName("ReaderTest6: inputTest3: length")
     void readerTest6(){
-        List inputTest1 = Reader.readFromInput("src/test/resources/inputTest3.txt");
+        List inputTest1 = Reader.readFromInput("src/test/resources/readerTest3.txt");
         int actual = inputTest1.size();
         int expected = 62;
         assertEquals(actual,expected);
