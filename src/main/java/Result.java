20afb80987f2cafc0b5e12d5cfa93a8af9a9da67
Bernhard Vogler <bernhard.vogler@stud.fh-campuswien.ac.at>
2021-03-18 20:13:35 +0100

    changed method to use list.size instead of given length variable added two tests (for this and one generic)

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,42 +1,50 @@
+import org.mockito.internal.matchers.Null;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Result {
     public static final int ROUNDINGBOUNDARY = 37;
-    public static final int UPPERBOUND = 60;
-
+    public static final int UPPERBOUNDARY = 60;
 
     public static List<Integer> gradingStudents(List<Integer> grades){
         // This method takes a list of grades (first entry has to be the arraylength of the following entries)
-        // If the list is either too short or too long null is returned otherwise the array is modified using modulo 5
-        // as was specified. The first entry (n) is removed during this process. It isn't needed -> use .size() instead
+        // If the list is either too short or too long null an Exception is thrown otherwise the array is modified
+        // using modulo 5 as was specified. The first entry (n) is ignored/input is not trusted
 
         List<Integer> results = new ArrayList<>();
 
         if(grades == null){
-            return null;
+            throw new NullPointerException("given array of grades is null");
         }
-        else if (grades.get(0) < 1 || grades.get(0) > UPPERBOUND){
-            return null;
+
+        if (grades.isEmpty() || grades.size() < 2){
+            throw new IllegalArgumentException("given array of grades is either empty or too short (only 'n' entry)");
+        }
+        else if (grades.size() > UPPERBOUNDARY+1){
+            throw new IllegalArgumentException("given array of grades is too long");
         }
         else{
-            for(int i = 1; i < grades.size(); i++){
-                if(grades.get(i) < 0 || grades.get(i) > 100){
+            for(int grade : grades){
+                if(grade < 0 || grade > 100){
                     results.add(-1);
                 }
-                else if(grades.get(i) > 0 && grades.get(i) < ROUNDINGBOUNDARY){
-                    results.add(grades.get(i));
+                else if(grade > 0 && grade < ROUNDINGBOUNDARY){
+                    results.add(grade);
                 }
-                else if((5 - grades.get(i) % 5) == 2){
-                    results.add(grades.get(i)+2);
+                else if( (5 - grade % 5) == 2){
+                    results.add(grade+2);
                 }
-                else if((5 - grades.get(i) % 5) == 1){
-                    results.add(grades.get(i)+1);
+                else if( (5 - grade % 5) == 1){
+                    results.add(grade+1);
                 }
                 else{
-                    results.add(grades.get(i));
+                    results.add(grade);
                 }
             }
+            results.remove(0);
         }
 
         return results;
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,38 +12,44 @@ public class ResultTest {
     // Link to Repository: https://github.com/BernhardV-GH/prog2-ss21-exercise1
 
     @Test
-    @DisplayName("Constraints Test 1: n < 1")
+    @DisplayName("Constraints Test 1: parameter 'grades' given is null")
     void constraintsTest1() {
-        List<Integer> input = Arrays.asList(0,1,2);
-        assertNull(Result.gradingStudents(input));
+        assertThrows(NullPointerException.class, () -> Result.gradingStudents(null));
     }
 
     @Test
-    @DisplayName("Constraints Test 2: n < 0")
+    @DisplayName("Constraints Test 2: n < 1")
     void constraintsTest2() {
-        List<Integer> input = Arrays.asList(-1,1,2);
-        assertNull(Result.gradingStudents(input));
+        List<Integer> input = Arrays.asList(0);
+        assertThrows(IllegalArgumentException.class, () -> Result.gradingStudents(input));
     }
 
     @Test
-    @DisplayName("Constraints Test 3: n > 60")
+    @DisplayName("Constraints Test 3: n < 0")
     void constraintsTest3() {
-        List<Integer> input = IntStream.range(1, 100).boxed().collect(Collectors.toList());
-        input.set(0, input.size());
-        assertNull(Result.gradingStudents(input));
+        List<Integer> input = new ArrayList<>();
+        assertThrows(IllegalArgumentException.class, () -> Result.gradingStudents(input));
     }
 
     @Test
-    @DisplayName("Constraints Test 4: grades[i] < 0 || grades [i] > 100")
+    @DisplayName("Constraints Test 4: n > 60")
     void constraintsTest4() {
+        List<Integer> input = IntStream.range(0, 100).boxed().collect(Collectors.toList());
+        assertThrows(IllegalArgumentException.class, () -> Result.gradingStudents(input));
+    }
+
+    @Test
+    @DisplayName("Constraints Test 5: n = 61")
+    void constraintsTest5() {
+        List<Integer> input = IntStream.range(0, 62).boxed().collect(Collectors.toList());
+        assertThrows(IllegalArgumentException.class, () -> Result.gradingStudents(input));
+    }
+
+    @Test
+    @DisplayName("Constraints Test 6: grades[i] < 0 || grades [i] > 100")
+    void constraintsTest6() {
         List<Integer> actual = Result.gradingStudents(Arrays.asList(5,-11,-1,55,100,111));
         assertEquals(Arrays.asList(-1,-1,55,100,-1), actual);
-    }
-
-    @Test
-    @DisplayName("Constraints Test 5: parameter 'grades' given is null")
-    void constraintsTest5() {
-        assertNull(Result.gradingStudents(null));
     }
 
     @Test
@@ -73,6 +81,14 @@ public class ResultTest {
     void gradingTest4() {
         List<Integer> actual = Result.gradingStudents(Arrays.asList(13,39,44,49,54,59,64,69,74,79,84,89,94,99));
         List<Integer> expected = Arrays.asList(40,45,50,55,60,65,70,75,80,85,90,95,100);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Grading Test 5 - generic test combining tests above")
+    void gradingTest5() {
+        List<Integer> actual = Result.gradingStudents(Arrays.asList(11,0,11,22,33,44,55,66,77,88,99,111));
+        List<Integer> expected = Arrays.asList(0,11,22,33,45,55,66,77,90,100,-1);
         assertEquals(expected, actual);
     }
 }
