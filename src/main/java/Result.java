e8e51a8a4b01c21976f07be0d24af8bff6f50bc9
Bernhard Vogler <bernhard.vogler@stud.fh-campuswien.ac.at>
2021-03-18 19:09:57 +0100

    removed unnecessary classes streamlined testing

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Result {
+    public static final int ROUNDINGBOUNDARY = 37;
+    public static final int UPPERBOUND = 60;
+
+
     public static List<Integer> gradingStudents(List<Integer> grades){
         // This method takes a list of grades (first entry has to be the arraylength of the following entries)
         // If the list is either too short or too long null is returned otherwise the array is modified using modulo 5
@@ -10,20 +14,17 @@ public class Result {
         List<Integer> results = new ArrayList<>();
 
         if(grades == null){
-            System.out.println("given list grades seems to be 'null'");
             return null;
         }
-        else if (grades.get(0) < 1 || grades.get(0) > 60){
-            System.out.println("file is either empty (n < 1) or too long (n > 60)");
+        else if (grades.get(0) < 1 || grades.get(0) > UPPERBOUND){
             return null;
         }
         else{
             for(int i = 1; i < grades.size(); i++){
                 if(grades.get(i) < 0 || grades.get(i) > 100){
-                    System.out.println("entry " + (i+1) + " is either '< 0' or '> 100'. Invalid entry set to '-1'");
                     results.add(-1);
                 }
-                else if(grades.get(i) > 0 && grades.get(i) < 40){
+                else if(grades.get(i) > 0 && grades.get(i) < ROUNDINGBOUNDARY){
                     results.add(grades.get(i));
                 }
                 else if((5 - grades.get(i) % 5) == 2){
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ResultTest {
     // Link to Repository: https://github.com/BernhardV-GH/prog2-ss21-exercise1
@@ -10,166 +12,67 @@ public class ResultTest {
     @Test
     @DisplayName("Constraints Test 1: n < 1")
     void constraintsTest1() {
-        List<Integer> input = new ArrayList<>();
-        input.add(0);
-        input.add(1);
-        input.add(2);
-        List<Integer> actual = Result.gradingStudents(input);
-        List<Integer> expected = null;
-        assertEquals(expected, actual);
+        List<Integer> input = Arrays.asList(0,1,2);
+        assertNull(Result.gradingStudents(input));
     }
+
     @Test
     @DisplayName("Constraints Test 2: n < 0")
     void constraintsTest2() {
-        List<Integer> input = new ArrayList<>();
-        input.add(-1);
-        input.add(1);
-        input.add(2);
-        List<Integer> actual = Result.gradingStudents(input);
-        List<Integer> expected = null;
-        assertEquals(expected, actual);
+        List<Integer> input = Arrays.asList(-1,1,2);
+        assertNull(Result.gradingStudents(input));
     }
+
     @Test
     @DisplayName("Constraints Test 3: n > 60")
     void constraintsTest3() {
-        List<Integer> input = new ArrayList<>();
-        for (int i = 1; i < 100; i++) {
-            input.add(i);
-        }
+        List<Integer> input = IntStream.range(1, 100).boxed().collect(Collectors.toList());
         input.set(0, input.size());
-        List<Integer> actual = Result.gradingStudents(input);
-        List<Integer> expected = null;
-        assertEquals(expected, actual);
+        assertNull(Result.gradingStudents(input));
     }
+
     @Test
     @DisplayName("Constraints Test 4: grades[i] < 0 || grades [i] > 100")
     void constraintsTest4() {
-        List<Integer> input = new ArrayList<>();
-        input.add(5);
-        input.add(-11);
-        input.add(-1);
-        input.add(55);
-        input.add(100);
-        input.add(111);
-
-        List<Integer> actual = Result.gradingStudents(input);
-
-        List<Integer> expected = new ArrayList<>();
-        expected.add(-1);
-        expected.add(-1);
-        expected.add(55);
-        expected.add(100);
-        expected.add(-1);
-
-        assertEquals(expected, actual);
+        List<Integer> actual = Result.gradingStudents(Arrays.asList(5,-11,-1,55,100,111));
+        assertEquals(Arrays.asList(-1,-1,55,100,-1), actual);
     }
+
     @Test
     @DisplayName("Constraints Test 5: parameter 'grades' given is null")
     void constraintsTest5() {
-        List<Integer> actual = Result.gradingStudents(null);
-        List<Integer> expected = null;
-        assertEquals(expected, actual);
+        assertNull(Result.gradingStudents(null));
     }
 
     @Test
-    @DisplayName("Grading Test 1 - no rounding < 40")
+    @DisplayName("Grading Test 1 - no rounding < 38")
     void gradingTest1() {
-        List<Integer> input = new ArrayList<>();
-        input.add(10);
-        input.add(1);
-        input.add(12);
-        input.add(23);
-        input.add(34);
-        input.add(5);
-        input.add(16);
-        input.add(27);
-        input.add(38);
-        input.add(9);
-        input.add(0);
-
+        List<Integer> input = Arrays.asList(10,1,12,23,34,5,16,27,37,9,0);
         List<Integer> actual = Result.gradingStudents(input);
-        input.remove(0);
-
-        assertEquals(input, actual);
+        assertEquals(Arrays.asList(1,12,23,34,5,16,27,37,9,0), actual);
     }
 
     @Test
     @DisplayName("Grading Test 2 - no rounding up if next multiple of 5 is '>= 3' away")
     void gradingTest2() {
-        List<Integer> input = new ArrayList<>();
-        input.add(6);
-        input.add(40);
-        input.add(51);
-        input.add(62);
-        input.add(75);
-        input.add(86);
-        input.add(97);
-
+        List<Integer> input = Arrays.asList(6,40,51,62,75,86,97);
         List<Integer> actual = Result.gradingStudents(input);
-        input.remove(0);
-
-        assertEquals(input, actual);
+        assertEquals(Arrays.asList(40,51,62,75,86,97), actual);
     }
 
     @Test
     @DisplayName("Grading Test 3 - rounding up if next multiple of 5 is 2 away")
     void gradingTest3() {
-        List<Integer> input = new ArrayList<>();
-        input.add(12);
-
-        input.add(43);
-        input.add(48);
-        input.add(53);
-        input.add(58);
-        input.add(63);
-        input.add(68);
-        input.add(73);
-        input.add(78);
-        input.add(83);
-        input.add(88);
-        input.add(93);
-        input.add(98);
-
-        List<Integer> expected = new ArrayList<>();
-        int tmp = 45;
-        for(int i = 0; i < 12; i++){
-            expected.add(tmp);
-            tmp+=5;
-        }
-
-        List<Integer> actual = Result.gradingStudents(input);
-
+        List<Integer> actual = Result.gradingStudents(Arrays.asList(13,38,43,48,53,58,63,68,73,78,83,88,93,98));
+        List<Integer> expected = Arrays.asList(40,45,50,55,60,65,70,75,80,85,90,95,100);
         assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("Grading Test 4 - rounding up if next multiple of 5 is 1 away")
     void gradingTest4() {
-        List<Integer> input = new ArrayList<>();
-        input.add(12);
-
-        input.add(44);
-        input.add(49);
-        input.add(54);
-        input.add(59);
-        input.add(64);
-        input.add(69);
-        input.add(74);
-        input.add(79);
-        input.add(84);
-        input.add(89);
-        input.add(94);
-        input.add(99);
-
-        List<Integer> expected = new ArrayList<>();
-        int tmp = 45;
-        for(int i = 0; i < 12; i++){
-            expected.add(tmp);
-            tmp+=5;
-        }
-
-        List<Integer> actual = Result.gradingStudents(input);
-
+        List<Integer> actual = Result.gradingStudents(Arrays.asList(13,39,44,49,54,59,64,69,74,79,84,89,94,99));
+        List<Integer> expected = Arrays.asList(40,45,50,55,60,65,70,75,80,85,90,95,100);
         assertEquals(expected, actual);
     }
 }
